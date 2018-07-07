41d5171eb3aaafc8df4d675fb525551279532c63
chenshenhai <cshenhai@gmail.com>
2018-07-07 15:34:42 +0800

    feat: update chapter-03-03-02

--- demo/chapter-03-03-02/index.js
+++ demo/chapter-03-03-02/index.js
@@ -3,31 +3,31 @@ let app = new Koa();
 
 function indirectMiddleware(path, middleware) {
   return async function(ctx, next) {
-    if ( ctx.path === path ) {
-      await middleware(ctx, next)
+    console.log(ctx.path === path, middleware);
+    if (ctx.path === path) {
+      await middleware(ctx, next);
+    } else {
+      await next();
     }
-  }
+  };
 }
 
-const index = async function ( ctx, next ) {
+const index = async function(ctx, next) {
   ctx.body = 'this is index page';
-  await next();
-}
+};
 
-const hello = async function ( ctx, next ) {
+const hello = async function(ctx, next) {
   ctx.body = 'this is hello page';
-  await next();
-}
+};
 
-const world = async function ( ctx, next ) {
+const world = async function(ctx, next) {
   ctx.body = 'this is world page';
-  await next();
-}
+};
 
-app.use( indirectMiddleware( '/', index ) );
-app.use( indirectMiddleware( '/hello', hello ) );
-app.use( indirectMiddleware( '/world', world ) );
+app.use(indirectMiddleware('/', index));
+app.use(indirectMiddleware('/hello', hello));
+app.use(indirectMiddleware('/world', world));
 
-app.listen(3001, function(){
+app.listen(3001, () => {
   console.log('the demo is start at port 3001');
-})
+});
